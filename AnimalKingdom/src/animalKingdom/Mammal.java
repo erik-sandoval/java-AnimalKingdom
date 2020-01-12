4d312b73549e17d074e008a7520ba2249aae31ca
Erik Sandoval <erik.sandoval@icloud.com>
2020-01-12 15:55:00 -0800

    Created Mammal class that extendAnimal

---
+++ AnimalKingdom/src/animalKingdom/Mammal.java
@@ -0,0 +1,20 @@
+package animalKingdom;
+
+public class Mammal extends Animal {
+
+    public Mammal(String name, int year) {
+        super(name, year);
+	}
+
+	public void move() {
+        System.out.println(getName() + " walks");
+    }
+
+    public void breathe() {
+        System.out.println(getName() + " breathe through their lungs");
+    }
+    
+    public void reproduce() {
+        System.out.println(getName() + " have live births");
+    }
+}
